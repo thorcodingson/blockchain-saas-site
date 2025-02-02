cf4b56aa590145d261fec3e4927a20c7d83057e7
Thor Codingson <markingmatt@gmail.com>
2025-02-02 13:05:20 +0100

    feature: Header component

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,10 @@
+export const HeroSection = () => {
+  return (
+    <section>
+      <div className="container">
+        <p>Intorucing Blockforge</p>
+        <h1>The Future of Blockchain is Here.</h1>
+      </div>
+    </section>
+  )
+}
