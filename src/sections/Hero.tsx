b990338ef2820a5c07ba0133bd11a436d55bf54d
Thor Codingson <markingmatt@gmail.com>
2025-02-02 13:24:04 +0100

    feature: CutCornerButton with custom class

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,16 @@
+import { CutCornerButton } from '../components/CutCornerButton'
+
 export const HeroSection = () => {
   return (
     <section>
       <div className="container">
         <p>Intorucing Blockforge</p>
         <h1>The Future of Blockchain is Here.</h1>
+        <p>
+          Blockforge is pioneering smart contract integrity with cutting-edge
+          data solutions
+        </p>
+        <CutCornerButton>Get started</CutCornerButton>
       </div>
     </section>
   )
